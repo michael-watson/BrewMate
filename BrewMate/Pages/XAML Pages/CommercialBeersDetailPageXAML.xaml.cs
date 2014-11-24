acb028264d62d46a30fbf7e05e61b16548e1f8c9
michael-watson <michael.watson@xamarin.com>
2014-11-24 12:25:51 -0800

    XAML

---
+++ BrewMate/Pages/XAML Pages/CommercialBeersDetailPageXAML.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace BrewMate
+{	
+	public partial class CommercialBeersDetailPageXAML : GreenGradientPage
+	{	
+		TableSection beerSection = new TableSection ();
+
+		public CommercialBeersDetailPageXAML (BeerStyle selected)
+		{
+			string[] commercialBeers = selected.CommercialExamples.Split (',');
+
+			InitializeComponent ();
+
+			LoadCommercialBeers (commercialBeers);
+		}
+
+		public void LoadCommercialBeers(string[] beers)
+		{
+			foreach (string beer in beers)
+			{
+				WhiteTextColorLabel beerName = new WhiteTextColorLabel { 
+					Text = beer, 
+					YAlign = TextAlignment.Center,
+					XAlign = TextAlignment.Center
+				};
+				beerSection.Add (new ViewCell{View = beerName});
+			}
+			tableRoot.Add (beerSection);
+		}
+	}
+}
---
+++ BrewMate/Pages/XAML Pages/HopDetailsPageXAML.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace BrewMate
+{	
+	public partial class HopDetailsPageXAML : GreenGradientPage
+	{	
+		TableSection hopSection = new TableSection ();
+
+		public HopDetailsPageXAML (Hops selected)
+		{
+			BindingContext = selected;
+
+			string[] substitutes = selected.Substitutes.Split (',');
+
+			InitializeComponent ();
+
+			LoadSubstitutes (substitutes);
+		}
+
+		private void LoadSubstitutes(string[] hopSubs)
+		{
+			foreach (string sub in hopSubs)
+			{
+				WhiteTextColorLabel beerName = new WhiteTextColorLabel { 
+					Text = sub, 
+					Font = Font.SystemFontOfSize (NamedSize.Medium),
+					YAlign = TextAlignment.Center,
+					XAlign = TextAlignment.Center
+				};
+				hopSection.Add (new ViewCell{View = beerName});
+			}
+			tableRoot.Add (hopSection);
+		}
+	}
+}
