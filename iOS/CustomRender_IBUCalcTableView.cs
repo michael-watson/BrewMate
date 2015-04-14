9bdebfd057c83c6c93722c66b4728ccadf0d9958
michael-watson <michael.watson@xamarin.com>
2015-04-13 23:09:53 -0700

    Updates to Platform Specific Code
    Remove IBUTableAdapter.cs from android project. This currently isn’t
    working. Will hold file in root local folder.
    
    Add comments to iOS IBUCalcTableView Custom Renderer

--- iOS/CustomRender_IBUCalcTableView.cs
+++ iOS/CustomRender_IBUCalcTableView.cs
@@ -23,11 +23,13 @@ namespace BrewMate.iOS
 		{
 			base.OnElementChanged (e);
 
+			//Create UITableViewController
 			table = new UITableViewController_IBUCalcTableView ();
-
+			//Create blank UIView
 			view = new UIView ();
-
+			//Add the UITableViewController to the UIView
 			view.Add (table.View);
+			//Add the UIView to the NativeView
 			NativeView.Add (view);
 
 		}
@@ -36,9 +38,11 @@ namespace BrewMate.iOS
 		{
 			base.LayoutSubviews ();
 
+			//Get the width and height of the custom render object
 			var width = base.NativeView.Frame.Width;
 			var height = base.NativeView.Frame.Height;
 
+			//Set the UITableViewController Frame and the UIView Frame
 			table.View.Frame = new CGRect (0, 0, width, height); 
 			view.Frame = new CGRect (0, 0, width, height);
 		}
